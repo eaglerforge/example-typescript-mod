5c10efde5b2a760ce3f91139a6e6f047b64456b3
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-03-03 11:19:11 +0330

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: {
-    main: "./src/main.ts",
+    main: "./src/Main.ts",
   },
   output: {
     path: path.resolve(__dirname, './out'),
