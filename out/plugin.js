3469f4e43574b06f4a45adc50f5eb084718ae19b
radmanplays <radmanplays@users.noreply.github.com>
2024-03-11 16:45:06 +0000

    Apply automatic changes

---
+++ out/plugin.js
@@ -0,0 +1,7 @@
+(() => {
+  // src/Main.ts
+  ModAPI.displayToChat({ msg: "hello, this is eaglerforge!" });
+  ModAPI.logger.loginfo({ string: "you can log a string to console" });
+  ModAPI.uwuify({ string: "and even uwuify a string?" });
+})();
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vc3JjL01haW4udHMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbIk1vZEFQSS5kaXNwbGF5VG9DaGF0KHttc2c6IFwiaGVsbG8sIHRoaXMgaXMgZWFnbGVyZm9yZ2UhXCJ9KVxuTW9kQVBJLmxvZ2dlci5sb2dpbmZvKHtzdHJpbmc6IFwieW91IGNhbiBsb2cgYSBzdHJpbmcgdG8gY29uc29sZVwifSlcbk1vZEFQSS51d3VpZnkoe3N0cmluZzogXCJhbmQgZXZlbiB1d3VpZnkgYSBzdHJpbmc/XCJ9KSJdLAogICJtYXBwaW5ncyI6ICI7O0FBQUEsU0FBTyxjQUFjLEVBQUMsS0FBSyw4QkFBNkIsQ0FBQztBQUN6RCxTQUFPLE9BQU8sUUFBUSxFQUFDLFFBQVEsa0NBQWlDLENBQUM7QUFDakUsU0FBTyxPQUFPLEVBQUMsUUFBUSw0QkFBMkIsQ0FBQzsiLAogICJuYW1lcyI6IFtdCn0K
