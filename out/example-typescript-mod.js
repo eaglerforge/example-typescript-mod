b50763d64823edcfb5a737e0c7ff2dff807cd2d4
radmanplays <radmanplays@users.noreply.github.com>
2024-03-13 06:48:35 +0000

    Apply automatic changes

---
+++ out/example-typescript-mod.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/Main.ts ***!
+  \*********************/
+ModAPI.displayToChat({ msg: "hello, this is eaglerforge!" });
+ModAPI.logger.loginfo({ string: "you can log a string to console" });
+ModAPI.uwuify({ string: "and even uwuify a string?" });
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZS10eXBlc2NyaXB0LW1vZC5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHVCQUF1QixvQ0FBb0M7QUFDM0Qsd0JBQXdCLDJDQUEyQztBQUNuRSxnQkFBZ0IscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZXhhbXBsZS10eXBlc2NyaXB0LW1vZC8uL3NyYy9NYWluLnRzIl0sInNvdXJjZXNDb250ZW50IjpbIk1vZEFQSS5kaXNwbGF5VG9DaGF0KHsgbXNnOiBcImhlbGxvLCB0aGlzIGlzIGVhZ2xlcmZvcmdlIVwiIH0pO1xuTW9kQVBJLmxvZ2dlci5sb2dpbmZvKHsgc3RyaW5nOiBcInlvdSBjYW4gbG9nIGEgc3RyaW5nIHRvIGNvbnNvbGVcIiB9KTtcbk1vZEFQSS51d3VpZnkoeyBzdHJpbmc6IFwiYW5kIGV2ZW4gdXd1aWZ5IGEgc3RyaW5nP1wiIH0pO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
