4be232ceaac1b320b300b2082cf2e04622df2491
0xerorn <92925112+0xerorn@users.noreply.github.com>
2024-07-30 19:14:12 +0300

    Delete src/util directory, we will replace with lib

--- src/util/chat.ts
+++
@@ -1,3 +0,0 @@
-export function displayToChat(message: string){
-    ModAPI.displayToChat({msg: message})
-}
--- src/util/uwuify.ts
+++
@@ -1,3 +0,0 @@
-export function uwuify(thestring: string){
-    ModAPI.uwuify({string: thestring})
-}
