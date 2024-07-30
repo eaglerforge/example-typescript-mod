6a2c36e6ac3c7ea2e21efe13ffe0b5a46dd052c4
0xerorn <0xerorn@users.noreply.github.com>
2024-07-30 16:56:58 +0000

    Apply automatic changes

---
+++ out/EaglerForge-dd8ej8wr.js
@@ -0,0 +1,4 @@
+// src/Main.ts
+ModAPI.displayToChat({ msg: "hello, this is eaglerforge!" });
+ModAPI.logger.loginfo({ string: "you can log a string to console" });
+ModAPI.uwuify({ string: "and even uwuify a string?" });
