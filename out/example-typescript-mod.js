e4e1d032123a564425d0612cb7ab7e7ac8cb589e
0xerorn <0xerorn@users.noreply.github.com>
2024-07-30 16:43:53 +0000

    Apply automatic changes

--- out/example-typescript-mod.js
+++ out/example-typescript-mod.js
@@ -1,11 +1,3 @@
-/******/ (() => { // webpackBootstrap
-/*!*********************!*\
-  !*** ./src/Main.ts ***!
-  \*********************/
-ModAPI.displayToChat({ msg: "hello, this is eaglerforge!" });
-ModAPI.logger.loginfo({ string: "you can log a string to console" });
-ModAPI.uwuify({ string: "and even uwuify a string?" });
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZS10eXBlc2NyaXB0LW1vZC5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsdUJBQXVCLG9DQUFvQztBQUMzRCx3QkFBd0IsMkNBQTJDO0FBQ25FLGdCQUFnQixxQ0FBcUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9leGFtcGxlLXR5cGVzY3JpcHQtbW9kLy4vc3JjL01haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiTW9kQVBJLmRpc3BsYXlUb0NoYXQoeyBtc2c6IFwiaGVsbG8sIHRoaXMgaXMgZWFnbGVyZm9yZ2UhXCIgfSk7XG5Nb2RBUEkubG9nZ2VyLmxvZ2luZm8oeyBzdHJpbmc6IFwieW91IGNhbiBsb2cgYSBzdHJpbmcgdG8gY29uc29sZVwiIH0pO1xuTW9kQVBJLnV3dWlmeSh7IHN0cmluZzogXCJhbmQgZXZlbiB1d3VpZnkgYSBzdHJpbmc/XCIgfSk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+/*! For license information please see example-typescript-mod.js.LICENSE.txt */
+ModAPI.displayToChat({msg:"hello, this is eaglerforge!"}),ModAPI.logger.loginfo({string:"you can log a string to console"}),ModAPI.uwuify({string:"and even uwuify a string?"});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZS10eXBlc2NyaXB0LW1vZC5qcyIsIm1hcHBpbmdzIjoiO0FBQUFBLE9BQU9DLGNBQWMsQ0FBRUMsSUFBSyxnQ0FDNUJGLE9BQU9HLE9BQU9DLFFBQVEsQ0FBRUMsT0FBUSxvQ0FDaENMLE9BQU9NLE9BQU8sQ0FBRUQsT0FBUSIsInNvdXJjZXMiOlsid2VicGFjazovL2V4YW1wbGUtdHlwZXNjcmlwdC1tb2QvLi9zcmMvTWFpbi50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJNb2RBUEkuZGlzcGxheVRvQ2hhdCh7IG1zZzogXCJoZWxsbywgdGhpcyBpcyBlYWdsZXJmb3JnZSFcIiB9KTtcbk1vZEFQSS5sb2dnZXIubG9naW5mbyh7IHN0cmluZzogXCJ5b3UgY2FuIGxvZyBhIHN0cmluZyB0byBjb25zb2xlXCIgfSk7XG5Nb2RBUEkudXd1aWZ5KHsgc3RyaW5nOiBcImFuZCBldmVuIHV3dWlmeSBhIHN0cmluZz9cIiB9KTtcbiJdLCJuYW1lcyI6WyJNb2RBUEkiLCJkaXNwbGF5VG9DaGF0IiwibXNnIiwibG9nZ2VyIiwibG9naW5mbyIsInN0cmluZyIsInV3dWlmeSJdLCJzb3VyY2VSb290IjoiIn0=
