88306ac0d26f9cd14c8e449dccdb2085d8aef046
BendieGames <bendiereal@gmail.com>
2025-04-07 08:19:22 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-ModAPI.displayToChat({msg: "hello, this is eaglerforge!"})
-ModAPI.logger.loginfo({string: "you can log a string to console"})
-ModAPI.uwuify({string: "and even uwuify a string?"})
+/// <reference path="ModAPI.d.ts" />
+ModAPI.displayToChat("hello, this is eaglerforge!")
+
