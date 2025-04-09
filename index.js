1428552cdc3098be3b52ce03142190734c02fa17
BendieGamer <bendiereal@gmail.com>
2025-04-09 17:46:22 -0400

    Okay I think I finished

--- index.js
+++ index.js
@@ -4,27 +4,50 @@ const fs = require('fs');
 const path = require('path');
 
 const sourceDir = path.join(__dirname, 'source', 'src');
-const tsconfigPath = path.join(__dirname, 'source', 'tsconfig.json');
-const destDir = process.cwd();
+const tsconfigPath = path.join(__dirname, 'source', 'tsconfig.json'); 
+const destDir = process.cwd(); 
 
-const filesToCopy = [
-    'main.ts',
-    'ModAPI.d.ts'
-];
 
-function copyFiles(src, dest, files) {
-    files.forEach(file => {
-        const srcFile = path.join(src, file);
-        const destFile = path.join(dest, file);
-        fs.copyFile(srcFile, destFile, err => {
+function copyDirectory(src, dest) {
+    fs.readdir(src, (err, files) => {
+        if (err) {
+            console.error(`Error reading directory ${src}: ${err}`);
+            return;
+        }
+        
+        fs.mkdir(dest, { recursive: true }, (err) => {
             if (err) {
-                console.error(`Error copying file ${file}: ${err}`);
-            } else {
-                console.log(`Copied ${file} to ${dest}`);
+                console.error(`Error creating directory ${dest}: ${err}`);
+                return;
             }
+
+            files.forEach(file => {
+                const srcFile = path.join(src, file);
+                const destFile = path.join(dest, file);
+
+                fs.stat(srcFile, (err, stats) => {
+                    if (err) {
+                        console.error(`Error stating file ${srcFile}: ${err}`);
+                        return;
+                    }
+
+                    if (stats.isDirectory()) {
+                        copyDirectory(srcFile, destFile); 
+                    } else {
+                        fs.copyFile(srcFile, destFile, err => {
+                            if (err) {
+                                console.error(`Error copying file ${file}: ${err}`);
+                            } else {
+                                console.log(`Copied ${file} to ${dest}`);
+                            }
+                        });
+                    }
+                });
+            });
         });
     });
 }
+
 
 function copyTsconfig(src, dest) {
     fs.copyFile(src, path.join(dest, 'tsconfig.json'), err => {
@@ -37,6 +60,6 @@ function copyTsconfig(src, dest) {
     });
 }
 
-console.log("Copying specified files from the source folder...");
-copyFiles(sourceDir, destDir, filesToCopy);
-copyTsconfig(tsconfigPath, destDir);
+console.log("Copying src folder and tsconfig.json from the source folder...");
+copyDirectory(sourceDir, path.join(destDir, 'src')); 
+copyTsconfig(tsconfigPath, destDir); 
